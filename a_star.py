f4cdd374ec99d03ede827c4a7ea0a19a0f58df02
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2020-07-12 05:05:05 +0000

    a* works, now I have to write tests

--- a_star.py
+++ a_star.py
@@ -1,5 +1,6 @@
 from Maze import Maze
 from dataclasses import dataclass
+from math import sqrt
 
 @dataclass
 class DataClassCell:
@@ -13,7 +14,7 @@ class DataClassCell:
 def a_star(maze, v=False):
     
     def calc_h(cell):
-        return abs(cell[0] - maze.end[0]) + abs(cell[1] - maze.end[1])
+        return ((cell[0] - maze.end[0])**2) + ((cell[1] - maze.end[1])**2)
 
     def make_path(data_cell):
         """ returns a list of the ordered path """
@@ -50,26 +51,28 @@ def a_star(maze, v=False):
             successor = (current_cell.cell[0] + l1[change], (current_cell.cell[1] + l1[(change + 1) % len(l1)]))
             # print(successor)
             if not (successor[0] < 0 or successor[0] > maze.width - 1 or successor[1] < 0 or successor[1] > maze.height - 1):
-                # print("inside")
                 if(successor not in closed_list):
                     # print("not in closed list")
-                    new_cell = DataClassCell(successor, current_cell, current_cell.g + 1, calc_h(successor), calc_h(successor) + current_cell.g + 1)
+                    new_g = current_cell.g + 1
+                    new_h = calc_h(successor)
+                    new_f = new_g + new_h
+                    new_cell = DataClassCell(successor, current_cell,  new_g, new_h, new_f)
                     if(successor not in open_list):
                         # print("not in open list")
                         if maze.grid[successor[1]][successor[0]] == 0:
-                            # print("is a 0")
-                            # adds the successor to the open list
                             if(maze.end == successor):
                                 return make_path(new_cell)
                             open_list[successor] = new_cell
                             if(v):
-                                maze.specials[successor] = str(new_cell.h)
+                                maze.specials[successor] = "o"
+                                # maze.specials[successor] = str(float(new_cell.f))
                     else:
                         if open_list[successor].parent.g > current_cell.g:
                             # changes the parent of the successor
                             open_list[successor] = new_cell
                             if(v):
-                                maze.specials[successor] = str(new_cell.h)
+                                maze.specials[successor] = "o"
+                                # maze.specials[successor] = str(float(new_cell.f))
     return "no path found"
                 
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from a_star import a_star
 
 if __name__ == "__main__":
     test_tuples = [(1,0), (1, 1), (0, 4), (1, 4), (2, 3), (3, 1), (3, 2), (3, 3), (3, 4)]
-    my_maze = Maze(5, 5, [(1, 0)], start=(0, 0))
+    my_maze = Maze(5, 5, [(1, 0), (2, 1), (3, 3), (1, 2)], start=(0, 0))
 
     print(my_maze)
     print(a_star(my_maze, True))
